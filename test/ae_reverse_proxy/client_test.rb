e03917b5909bd020000bc85a9f85db91d113c3d7
Stanley Phu <stanley.phu@appfolio.com>
2021-01-28 17:55:15 -0800

    Include url params when forwarding request

--- test/ae_reverse_proxy/client_test.rb
+++ test/ae_reverse_proxy/client_test.rb
@@ -52,9 +52,41 @@ module AeReverseProxy
       client.forward_request(test_env)
     end
 
+    def test_forward_request__get_request_with_url_params
+      uri_str = 'https://www.appfoleo.com:8080'
+      proxy_uri = Addressable::URI.parse(uri_str)
+
+      client = Client.new(proxy_uri) do |config|
+        config.on_success do |status_code, target_response|
+          assert_equal 200, status_code
+          assert_equal 'Everything is A-ok', target_response.body
+        end
+      end
+
+      stub_request(:get, 'https://www.appfoleo.com:8080/posts?sort=desc')
+        .with(
+          headers: {
+            'Content-Length' => '0',
+            'Host' => 'www.appfoleo.com:8080',
+            'Origin' => 'https://www.appfoleo.com:8080',
+            'User-Agent' => 'Rails Testing',
+          },
+        ).to_return(status: 200, body: 'Everything is A-ok', headers: {})
+
+      client.forward_request(test_env_with_url_params)
+    end
+
     private
 
     def test_env(env_opts = {})
+      Rack::MockRequest.env_for('/posts', {
+        'HTTP_HOST' => 'test.host'.b,
+        'REMOTE_ADDR' => '0.0.0.0'.b,
+        'HTTP_USER_AGENT' => 'Rails Testing'.b,
+      }.merge(env_opts))
+    end
+
+    def test_env_with_url_params(env_opts = {})
       Rack::MockRequest.env_for('/posts?sort=desc', {
         'HTTP_HOST' => 'test.host'.b,
         'REMOTE_ADDR' => '0.0.0.0'.b,
