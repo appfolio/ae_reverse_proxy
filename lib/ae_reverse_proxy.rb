74813faa97784a63342ee8b88e0f995eef98a27a
Joel Bandi <joelvivekbandi@gmail.com>
2021-02-01 00:01:56 -0800

    bump minor veersion

--- lib/ae_reverse_proxy.rb
+++ lib/ae_reverse_proxy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module AeReverseProxy
-  VERSION = '1.0.0'
+  VERSION = '1.1.0'
   autoload :Client, 'ae_reverse_proxy/client'
   autoload :ControllerCallbackMethod, 'ae_reverse_proxy/controller_callback_method'
 end
