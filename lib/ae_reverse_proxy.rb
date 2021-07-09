40bcc500410d4f866a69c6e2248e89a42174f120
Nicholas Lee <nicholas.lee@appfolio.com>
2021-07-09 14:11:56 -0700

    Move `version` to its own file, update `gemspec`

--- lib/ae_reverse_proxy.rb
+++ lib/ae_reverse_proxy.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module AeReverseProxy
-  VERSION = '2.0.0'
   autoload :Client, 'ae_reverse_proxy/client'
   autoload :ControllerCallbackMethod, 'ae_reverse_proxy/controller_callback_method'
 end
